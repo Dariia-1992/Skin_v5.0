01923d6643cb6a88875ad6a775a07dab3c9c897b
Dariia <pers4040@gmail.com>
2020-06-06 00:24:03 +0300

    add toolbar

--- app/src/main/java/com/example/skin_v5/MainActivity.java
+++ app/src/main/java/com/example/skin_v5/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.skin_v5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+
+import com.example.skin_v5.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainFragment fragment = new MainFragment();
+
+        FragmentManager manager = getSupportFragmentManager();
+        if (manager == null)
+            return;
+
+        manager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
--- app/src/main/java/com/example/skin_v5/fragment/MainFragment.java
+++ app/src/main/java/com/example/skin_v5/fragment/MainFragment.java
@@ -7,16 +7,57 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import com.example.skin_v5.R;
 
 public class MainFragment extends Fragment {
 
+    private View view;
+    private String searchStr;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        view = inflater.inflate(R.layout.fragment_main, container, false);
+        View searchButton = view.findViewById(R.id.toolbar_search);
+        searchButton.setOnClickListener(v -> setupSearch(true));
+
+        setupSearch(false);
+
         return view;
+    }
+
+    private void setupSearch(boolean show) {
+        View searchButton = view.findViewById(R.id.toolbar_search);
+        View toolbarText = view.findViewById(R.id.toolbar_name);
+        SearchView searchView = view.findViewById(R.id.search);
+
+        searchView.setOnCloseListener(() -> {
+            setupSearch(false);
+            //whatCurrent();
+            return false;
+        });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) { return false; }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchStr = newText;
+                //filterData();
+                return false;
+            }
+        });
+        searchButton.setVisibility(show ? View.GONE : View.VISIBLE);
+        toolbarText.setVisibility(show ? View.GONE : View.VISIBLE);
+        searchView.setVisibility(show ? View.VISIBLE : View.GONE);
+
+        if (show) {
+            searchView.setIconified(false);
+            searchView.requestFocusFromTouch();
+        }
     }
 }
